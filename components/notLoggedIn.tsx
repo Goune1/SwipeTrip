54b7f86f360ca507ef5093718d65f5a2c8cdbf2e
Goune <71518163+Goune1@users.noreply.github.com>
2024-06-23 17:55:18 +0200

    Add files via upload

--- components/notLoggedIn.tsx
+++ components/notLoggedIn.tsx
@@ -16,7 +16,7 @@ export default function notLoggedIn() {
                 href="/"
                 className="rounded-md bg-indigo-600 mt-4 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
-            Page d'accueil
+            Page d&apos;accueil
             </a>
         </div>
       </div> 

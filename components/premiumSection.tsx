697cd02562ec4c95ca7bdd55dfb89766bf7f195f
Goune <71518163+Goune1@users.noreply.github.com>
2024-06-23 22:00:35 +0200

    Update premiumSection.tsx

--- components/premiumSection.tsx
+++ components/premiumSection.tsx
@@ -85,7 +85,7 @@ export default function Premium() {
                                     </a>
                                 ) : (
                                     <a
-                                    href='login'
+                                    href='signup'
                                     className="mt-10 block w-full rounded-md bg-fuchsia-700 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-fuchsia-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 cursor-pointer"
                                     >
                                         Obtenir le Premium

7fae3b2a905bb8c6565bda0a62b5a418e4ca936f
Goune <71518163+Goune1@users.noreply.github.com>
2024-06-23 21:00:59 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,13 +15,15 @@ export default function RootLayout({
   return (
     <html lang="fr">
       <link rel="icon" type="image/svg+xml" href="logo.png" />
-      <!-- Google Tag Manager -->
-      <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-      })(window,document,'script','dataLayer','GTM-KNN3D92V');</script>
-      <!-- End Google Tag Manager -->
+      <head>
+        <!-- Google Tag Manager -->
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-KNN3D92V');</script>
+        <!-- End Google Tag Manager -->
+      </head>
       <body className={GeistSans.className}>
         <!-- Google Tag Manager (noscript) -->
         <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KNN3D92V"

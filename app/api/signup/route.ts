feaa697989c085389e4b8ea1e7162b4a20e1d9ec
Goune <71518163+Goune1@users.noreply.github.com>
2024-06-24 10:14:10 +0200

    Update route.ts

--- app/api/signup/route.ts
+++ app/api/signup/route.ts
@@ -8,7 +8,7 @@ export async function POST(request: Request): Promise<NextResponse> {
 
     const loginCollection = require("@/schemas/login");
 
-    const DB_URL = process.env.DB_URL;
+    const DB_URL = process.env.MONGO_URL
     
     async function connectToDatabase() {
         try {
